862d9c0f51ebc19744ccdd8b26375609d19368c5
baijuxavior <baijuxavior@gmail.com>
2019-08-07 13:00:33 +0530

    [Added] - Revenue collection Excel format

--- Fingerprint Information System/frmRevenueCollection.Designer.vb
+++ Fingerprint Information System/frmRevenueCollection.Designer.vb
@@ -28,12 +28,14 @@ Partial Class frmRevenueCollection
         Me.LabelX3 = New DevComponents.DotNetBar.LabelX()
         Me.LabelX4 = New DevComponents.DotNetBar.LabelX()
         Me.cmbMonth = New DevComponents.DotNetBar.Controls.ComboBoxEx()
-        Me.btnGenerate = New DevComponents.DotNetBar.ButtonX()
+        Me.btnOpenFolder = New DevComponents.DotNetBar.ButtonX()
         Me.FingerPrintDataSet = New FingerprintInformationSystem.FingerPrintDataSet()
         Me.FPAttestationRegisterBindingSource = New System.Windows.Forms.BindingSource(Me.components)
         Me.FPARegisterTableAdapter = New FingerprintInformationSystem.FingerPrintDataSetTableAdapters.FPAttestationRegisterTableAdapter()
         Me.LabelX1 = New DevComponents.DotNetBar.LabelX()
         Me.txtHeadofAccount = New DevComponents.DotNetBar.Controls.TextBoxX()
+        Me.btnGenerateExcel = New DevComponents.DotNetBar.ButtonX()
+        Me.LabelX2 = New DevComponents.DotNetBar.LabelX()
         CType(Me.txtYear, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.FingerPrintDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.FPAttestationRegisterBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -50,13 +52,13 @@ Partial Class frmRevenueCollection
         Me.txtYear.FocusHighlightEnabled = True
         Me.txtYear.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtYear.InputHorizontalAlignment = DevComponents.Editors.eHorizontalAlignment.Left
-        Me.txtYear.Location = New System.Drawing.Point(295, 8)
+        Me.txtYear.Location = New System.Drawing.Point(313, 8)
         Me.txtYear.MaxValue = 2099
         Me.txtYear.MinValue = 0
         Me.txtYear.Name = "txtYear"
         Me.txtYear.ShowUpDown = True
         Me.txtYear.Size = New System.Drawing.Size(73, 29)
-        Me.txtYear.TabIndex = 27
+        Me.txtYear.TabIndex = 1
         Me.txtYear.Value = 1900
         Me.txtYear.WatermarkText = "Year"
         '
@@ -82,7 +84,7 @@ Partial Class frmRevenueCollection
         '
         Me.LabelX4.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
         Me.LabelX4.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelX4.Location = New System.Drawing.Point(259, 13)
+        Me.LabelX4.Location = New System.Drawing.Point(277, 13)
         Me.LabelX4.Name = "LabelX4"
         Me.LabelX4.Size = New System.Drawing.Size(30, 20)
         Me.LabelX4.TabIndex = 29
@@ -98,24 +100,24 @@ Partial Class frmRevenueCollection
         Me.cmbMonth.ForeColor = System.Drawing.Color.Black
         Me.cmbMonth.FormattingEnabled = True
         Me.cmbMonth.ItemHeight = 23
-        Me.cmbMonth.Location = New System.Drawing.Point(117, 8)
+        Me.cmbMonth.Location = New System.Drawing.Point(135, 8)
         Me.cmbMonth.MaxDropDownItems = 15
         Me.cmbMonth.MaxLength = 255
         Me.cmbMonth.Name = "cmbMonth"
         Me.cmbMonth.Size = New System.Drawing.Size(136, 29)
-        Me.cmbMonth.TabIndex = 26
+        Me.cmbMonth.TabIndex = 0
         Me.cmbMonth.WatermarkText = "Month"
         '
-        'btnGenerate
+        'btnOpenFolder
         '
-        Me.btnGenerate.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
-        Me.btnGenerate.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
-        Me.btnGenerate.Location = New System.Drawing.Point(250, 87)
-        Me.btnGenerate.Name = "btnGenerate"
-        Me.btnGenerate.Size = New System.Drawing.Size(118, 50)
-        Me.btnGenerate.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
-        Me.btnGenerate.TabIndex = 30
-        Me.btnGenerate.Text = "Generate"
+        Me.btnOpenFolder.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.btnOpenFolder.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.btnOpenFolder.Location = New System.Drawing.Point(268, 85)
+        Me.btnOpenFolder.Name = "btnOpenFolder"
+        Me.btnOpenFolder.Size = New System.Drawing.Size(118, 50)
+        Me.btnOpenFolder.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.btnOpenFolder.TabIndex = 4
+        Me.btnOpenFolder.Text = "Open Folder"
         '
         'FingerPrintDataSet
         '
@@ -156,20 +158,47 @@ Partial Class frmRevenueCollection
         Me.txtHeadofAccount.DisabledBackColor = System.Drawing.Color.White
         Me.txtHeadofAccount.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtHeadofAccount.ForeColor = System.Drawing.Color.Black
-        Me.txtHeadofAccount.Location = New System.Drawing.Point(117, 50)
+        Me.txtHeadofAccount.Location = New System.Drawing.Point(135, 50)
         Me.txtHeadofAccount.Name = "txtHeadofAccount"
         Me.txtHeadofAccount.PreventEnterBeep = True
         Me.txtHeadofAccount.Size = New System.Drawing.Size(251, 29)
-        Me.txtHeadofAccount.TabIndex = 32
+        Me.txtHeadofAccount.TabIndex = 2
+        '
+        'btnGenerateExcel
+        '
+        Me.btnGenerateExcel.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.btnGenerateExcel.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.btnGenerateExcel.Location = New System.Drawing.Point(135, 85)
+        Me.btnGenerateExcel.Name = "btnGenerateExcel"
+        Me.btnGenerateExcel.Size = New System.Drawing.Size(118, 50)
+        Me.btnGenerateExcel.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.btnGenerateExcel.TabIndex = 3
+        Me.btnGenerateExcel.Text = "Excel Format"
+        '
+        'LabelX2
+        '
+        Me.LabelX2.AutoSize = True
+        '
+        '
+        '
+        Me.LabelX2.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelX2.Location = New System.Drawing.Point(6, 100)
+        Me.LabelX2.Name = "LabelX2"
+        Me.LabelX2.Size = New System.Drawing.Size(125, 20)
+        Me.LabelX2.TabIndex = 34
+        Me.LabelX2.Text = "Generate Statement"
         '
         'frmRevenueCollection
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(370, 138)
+        Me.ClientSize = New System.Drawing.Size(389, 138)
+        Me.Controls.Add(Me.LabelX2)
+        Me.Controls.Add(Me.btnGenerateExcel)
         Me.Controls.Add(Me.txtHeadofAccount)
         Me.Controls.Add(Me.LabelX1)
-        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.btnOpenFolder)
         Me.Controls.Add(Me.txtYear)
         Me.Controls.Add(Me.LabelX3)
         Me.Controls.Add(Me.LabelX4)
@@ -197,10 +226,12 @@ Partial Class frmRevenueCollection
     Friend WithEvents LabelX3 As DevComponents.DotNetBar.LabelX
     Friend WithEvents LabelX4 As DevComponents.DotNetBar.LabelX
     Friend WithEvents cmbMonth As DevComponents.DotNetBar.Controls.ComboBoxEx
-    Friend WithEvents btnGenerate As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents btnOpenFolder As DevComponents.DotNetBar.ButtonX
     Friend WithEvents FingerPrintDataSet As FingerprintInformationSystem.FingerPrintDataSet
     Friend WithEvents FPAttestationRegisterBindingSource As System.Windows.Forms.BindingSource
     Friend WithEvents FPARegisterTableAdapter As FingerprintInformationSystem.FingerPrintDataSetTableAdapters.FPAttestationRegisterTableAdapter
     Friend WithEvents LabelX1 As DevComponents.DotNetBar.LabelX
     Friend WithEvents txtHeadofAccount As DevComponents.DotNetBar.Controls.TextBoxX
+    Friend WithEvents btnGenerateExcel As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents LabelX2 As DevComponents.DotNetBar.LabelX
 End Class
--- Fingerprint Information System/frmRevenueCollection.vb
+++ Fingerprint Information System/frmRevenueCollection.vb
@@ -2,6 +2,7 @@
 Imports DevComponents.DotNetBar.Rendering ' to use office 2007 style forms
 Imports DevComponents.DotNetBar.Controls
 Imports Microsoft.Office.Interop
+Imports Microsoft.Office.Interop.Excel
 
 Public Class frmRevenueCollection
 
@@ -34,7 +35,7 @@ Public Class frmRevenueCollection
 
     End Sub
 
-    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs)
         TemplateFile = strAppUserPath & "\WordTemplates\RevenueCollection.docx"
         If My.Computer.FileSystem.FileExists(TemplateFile) = False Then
             MessageBoxEx.Show("File missing. Please re-install the Application.", strAppName, MessageBoxButtons.OK, MessageBoxIcon.Error)
@@ -93,44 +94,44 @@ Public Class frmRevenueCollection
                 d1 = New Date(y, 4, 1) 'april 1
 
                 amount2 = Val(Me.FPARegisterTableAdapter.AmountRemitted(d1, d2))
-                End If
+            End If
 
-                wdBooks("Amount2").Range.Text = amount2 & "/-"
+            wdBooks("Amount2").Range.Text = amount2 & "/-"
 
-                Dim amount3 As Integer = amount1 + amount2
-                wdBooks("Amount3").Range.Text = amount3 & "/-"
+            Dim amount3 As Integer = amount1 + amount2
+            wdBooks("Amount3").Range.Text = amount3 & "/-"
 
 
-                m = Me.cmbMonth.SelectedIndex + 1 ' selected month
-                y = Me.txtYear.Value - 1 'previous year
-                d = Date.DaysInMonth(y, m)
+            m = Me.cmbMonth.SelectedIndex + 1 ' selected month
+            y = Me.txtYear.Value - 1 'previous year
+            d = Date.DaysInMonth(y, m)
 
-                d2 = New Date(y, m, d) ' selected month of last year
+            d2 = New Date(y, m, d) ' selected month of last year
 
-                Dim amount4 As Integer = 0
+            Dim amount4 As Integer = 0
 
             If m < 4 Then
                 y = y - 1
             End If
 
-                d1 = New Date(y, 4, 1) 'april 1
+            d1 = New Date(y, 4, 1) 'april 1
 
-                amount4 = Val(Me.FPARegisterTableAdapter.AmountRemitted(d1, d2))
+            amount4 = Val(Me.FPARegisterTableAdapter.AmountRemitted(d1, d2))
 
 
-                wdBooks("Amount4").Range.Text = amount4 & "/-"
+            wdBooks("Amount4").Range.Text = amount4 & "/-"
 
-                wdApp.Visible = True
-                wdApp.Activate()
-                wdApp.WindowState = Word.WdWindowState.wdWindowStateMaximize
-                wdDoc.Activate()
+            wdApp.Visible = True
+            wdApp.Activate()
+            wdApp.WindowState = Word.WdWindowState.wdWindowStateMaximize
+            wdDoc.Activate()
 
-                ReleaseObject(wdBooks)
-                ReleaseObject(wdDoc)
-                ReleaseObject(wdDocs)
-                wdApp = Nothing
-                Me.Cursor = Cursors.Default
-                Me.Close()
+            ReleaseObject(wdBooks)
+            ReleaseObject(wdDoc)
+            ReleaseObject(wdDocs)
+            wdApp = Nothing
+            Me.Cursor = Cursors.Default
+            Me.Close()
         Catch ex As Exception
             Me.Cursor = Cursors.Default
             ShowErrorMessage(ex)
@@ -138,4 +139,226 @@ Public Class frmRevenueCollection
     End Sub
 
   
+    
+    Private Sub btnGenerateExcel_Click(sender As Object, e As EventArgs) Handles btnGenerateExcel.Click
+        Try
+            Me.Cursor = Cursors.WaitCursor
+
+            Dim sFileName As String = FileIO.SpecialDirectories.MyDocuments & "\Revenue Collection Statement - SDFPB " & ShortDistrictName & " - " & Me.cmbMonth.Text.ToUpper & " " & Me.txtYear.Text & ".xlsx"
+
+            If My.Computer.FileSystem.FileExists(sFileName) Then
+                Shell("explorer.exe " & sFileName, AppWinStyle.MaximizedFocus)
+                Me.Cursor = Cursors.Default
+                Me.Close()
+                Exit Sub
+            End If
+
+            ShowPleaseWaitForm()
+            Dim xlApp As Excel.Application = New Excel.Application()
+            Dim xlBooks As Excel.Workbooks = xlApp.Workbooks
+            Dim xlBook As Excel._Workbook = xlBooks.Add
+            Dim xlSheets As Excel.Sheets = xlBook.Worksheets
+            Dim xlSheet As Excel.Worksheet = xlBook.ActiveSheet
+
+            xlSheet.PageSetup.LeftMargin = 40
+            xlSheet.PageSetup.RightMargin = 25
+
+            xlSheet.Range("A1").HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter
+            xlSheet.Range("A1").Font.Bold = True
+            xlSheet.Range("A1").Font.Underline = Excel.XlUnderlineStyle.xlUnderlineStyleSingle
+
+            xlSheet.Range("A1").Value = "CoB Message"
+            xlSheet.Range("A1", "F1").Merge()
+
+            xlSheet.Range("A3").Value = "FROM: TESTER INSPECTOR, " & ShortOfficeName.ToUpper & ", " & FullDistrictName.ToUpper
+            xlSheet.Range("A3", "F3").Merge()
+
+            xlSheet.Range("A5").Value = "TO: THE DIRECTOR, FINGERPRINT BUREAU, THIRUVANANTHAPURAM"
+            xlSheet.Range("A5", "F5").Merge()
+
+            xlSheet.Range("A7:F7").Borders.LineStyle = 1
+            xlSheet.Range("A7").HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter
+            xlSheet.Range("A7").Font.Bold = True
+            xlSheet.Range("A7").Value = "No. " & PdlFPAttestation & "/PDL/" & Year(Today) & "/" & ShortOfficeName & "/" & ShortDistrictName & vbTab & vbTab & vbTab & vbTab & "Date:       /" & GenerateDateWithoutDay()
+            xlSheet.Range("A7", "F7").Merge()
+
+            xlSheet.Range("A9").Font.Bold = True
+            xlSheet.Range("A9").Value = "REVENUE INCOME DETAILS FOR THE MONTH OF " & Me.cmbMonth.Text.ToUpper & " " & Me.txtYear.Text
+            xlSheet.Range("A9", "F9").Merge()
+
+            With xlSheet.Range("A11", "F11")
+                .Font.Bold = True
+                .HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter
+            End With
+
+            xlSheet.Columns("A").ColumnWidth = 5
+            xlSheet.Columns("B").ColumnWidth = 15
+            xlSheet.Columns("C").ColumnWidth = 23
+            xlSheet.Columns("D").ColumnWidth = 19
+            xlSheet.Columns("E").ColumnWidth = 13
+            xlSheet.Columns("F").ColumnWidth = 14
+
+
+            xlSheet.Range("A11").Value = "Sl.No."
+            xlSheet.Range("B11").Value = "Head of Account"
+            xlSheet.Range("C11").Value = "Treasury"
+            xlSheet.Range("D11").Value = "Chalan No."
+            xlSheet.Range("E11").Value = "Date"
+            xlSheet.Range("F11").Value = "Amount"
+
+            Dim m = Me.cmbMonth.SelectedIndex + 1
+            Dim y = Me.txtYear.Value
+            Dim d As Integer = Date.DaysInMonth(y, m)
+            Dim d1 As Date = New Date(y, m, 1)
+            Dim d2 As Date = New Date(y, m, d)
+
+            Me.FPARegisterTableAdapter.FillByDateBetween(Me.FingerPrintDataSet.FPAttestationRegister, d1, d2)
+            Dim RowCount = Me.FingerPrintDataSet.FPAttestationRegister.Count
+            Dim i = 12
+
+            If RowCount = 0 Then
+                xlSheet.Cells(i, 6).value = "Rs. 0/-"
+            Else
+                For i = 12 To RowCount + i - 1
+                    Dim j = i - 12
+                    xlSheet.Cells(i, 1).value = j + 1
+                    xlSheet.Cells(i, 2).value = Me.FingerPrintDataSet.FPAttestationRegister(j).HeadOfAccount
+                    xlSheet.Cells(i, 3).value = Me.FingerPrintDataSet.FPAttestationRegister(j).Treasury
+                    xlSheet.Cells(i, 4).value = Me.FingerPrintDataSet.FPAttestationRegister(j).ChalanNumber
+                    xlSheet.Cells(i, 5).value = Me.FingerPrintDataSet.FPAttestationRegister(j).ChalanDate
+                    xlSheet.Cells(i, 6).value = Me.FingerPrintDataSet.FPAttestationRegister(j).AmountRemitted
+                Next
+
+                xlSheet.Cells(i, 5).font.bold = True
+                xlSheet.Cells(i, 5).value = "Total Rs."
+                xlSheet.Cells(i, 6).font.bold = True
+                xlSheet.Cells(i, 6).value = Me.FPARegisterTableAdapter.AmountRemitted(d1, d2).ToString
+
+                xlSheet.Range("A11:F" & i).Borders.LineStyle = 1
+            End If
+
+            i = i + 4
+
+            With xlSheet.Range("A" & i, "F" & i)
+                .Font.Bold = True
+                .HorizontalAlignment = Excel.XlVAlign.xlVAlignCenter
+                .VerticalAlignment = Excel.XlVAlign.xlVAlignTop
+            End With
+
+            xlSheet.Range("A" & i).Value = "Head of Account"
+            xlSheet.Range("C" & i).Value = "Amount collected during the month"
+            xlSheet.Range("D" & i).Value = "Amount collected upto the month in current financial year"
+            xlSheet.Range("E" & i).Value = "Progressive Total"
+            xlSheet.Range("F" & i).Value = "Collection from April upto the month during the last financial year"
+
+            xlSheet.Range("A" & i).WrapText = True
+            xlSheet.Range("C" & i).WrapText = True
+            xlSheet.Range("D" & i).WrapText = True
+            xlSheet.Range("E" & i).WrapText = True
+            xlSheet.Range("F" & i).WrapText = True
+
+            xlSheet.Range("A" & i, "B" & i).Merge()
+
+
+            My.Computer.Registry.SetValue(strGeneralSettingsPath, "HeadOfAccount", Me.txtHeadofAccount.Text, Microsoft.Win32.RegistryValueKind.String)
+
+            i = i + 1
+
+            xlSheet.Range("A" & i).Value = Me.txtHeadofAccount.Text
+
+            m = Me.cmbMonth.SelectedIndex + 1 ' selected month
+            y = Me.txtYear.Value
+            d = Date.DaysInMonth(y, m)
+            d1 = New Date(y, m, 1)
+            d2 = New Date(y, m, d)
+
+            Dim amount1 As Integer = Val(Me.FPARegisterTableAdapter.AmountRemitted(d1, d2)) 'current month
+            xlSheet.Range("C" & i).Value = amount1
+
+            Dim amount2 As Integer = 0
+
+            If m = 4 Then ' if april then previous amount is zero
+                amount2 = 0 'previous amount
+            Else
+                m = m - 1 'previous month
+                If m = 0 Then
+                    m = 12
+                    y = y - 1
+                End If
+
+                d = Date.DaysInMonth(y, m)
+                d2 = New Date(y, m, d) 'previous month
+
+                If m < 3 Then
+                    y = y - 1
+                End If
+
+                d1 = New Date(y, 4, 1) 'april 1
+
+                amount2 = Val(Me.FPARegisterTableAdapter.AmountRemitted(d1, d2))
+            End If
+
+            xlSheet.Range("D" & i).Value = amount2
+
+            xlSheet.Range("E" & i).Value = amount1 + amount2
+
+            m = Me.cmbMonth.SelectedIndex + 1 ' selected month
+            y = Me.txtYear.Value - 1 'previous year
+            d = Date.DaysInMonth(y, m)
+
+            d2 = New Date(y, m, d) ' selected month of last year
+
+            Dim amount4 As Integer = 0
+
+            If m < 4 Then
+                y = y - 1
+            End If
+
+            d1 = New Date(y, 4, 1) 'april 1
+
+            amount4 = Val(Me.FPARegisterTableAdapter.AmountRemitted(d1, d2))
+
+
+            xlSheet.Range("F" & i).Value = amount4
+
+            xlSheet.Range("A" & i - 1 & ":F" & i).Borders.LineStyle = 1
+
+            xlSheet.Range("A" & i, "B" & i).Merge()
+
+
+            If Not FileInUse(sFileName) Then xlBook.SaveAs(sFileName, Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook)
+
+            ClosePleaseWaitForm()
+
+            Me.Cursor = Cursors.Default
+            xlApp.Visible = True
+            xlApp.UserControl = True
+
+
+            xlSheet = Nothing
+            xlSheets = Nothing
+            xlBooks = Nothing
+
+            Me.Close()
+        Catch ex As Exception
+            Me.Cursor = Cursors.Default
+            ClosePleaseWaitForm()
+            ShowErrorMessage(ex)
+        End Try
+    End Sub
+
+    Private Sub btnOpenFolder_Click(sender As Object, e As EventArgs) Handles btnOpenFolder.Click
+        Me.Cursor = Cursors.WaitCursor
+
+        Dim sFileName As String = FileIO.SpecialDirectories.MyDocuments & "\Revenue Collection Statement - SDFPB " & ShortDistrictName & " - " & Me.cmbMonth.Text.ToUpper & " " & Me.txtYear.Text & ".xlsx"
+
+        If My.Computer.FileSystem.FileExists(sFileName) Then
+            Call Shell("explorer.exe /select," & sFileName, AppWinStyle.NormalFocus)
+
+        Else
+            Call Shell("explorer.exe " & FileIO.SpecialDirectories.MyDocuments, AppWinStyle.NormalFocus)
+        End If
+
+        Me.Cursor = Cursors.Default
+    End Sub
 End Class
