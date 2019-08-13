16baf193160ee743d46e412293ad9dfbe0f57c73
baijuxavior <baijuxavior@gmail.com>
2019-08-13 12:09:31 +0530

    [Fixed] - Settings Wizard not showing on install

--- Fingerprint Information System/My Project/Application.Designer.vb
+++ Fingerprint Information System/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.FingerprintInformationSystem.frmMainInterface
+            Me.MainForm = Global.FingerprintInformationSystem.FrmSettingsWizard
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
